4c75d455c7b7569a18899b1beff81cc55c8ed076
Manolo Arranz <manolo@tocandotecla.com>
2012-11-25 23:26:55 +0100

    Painted calculator in learning version

--- abacus.js
+++ abacus.js
@@ -10,14 +10,15 @@ var canvas = document.getElementById('canvas'),
 	valueElement = document.getElementById('value'),
 	DISTANCE_RODS = 60, 
 	width = DISTANCE_RODS * (numberOfRods + 1 ), 
-	MARGIN = 40,
+	MARGIN_TOP = 50,
+	MARGIN_LEFT = 300,
 	FRAME_LINE_WIDTH = 10, 
 	ROD_STROKE_STYLE = 'rgba(212,85,0,0.5)', 
 	ROD_LINE_WIDTH = 6, 
 	DOT_STROKE_STYLE = 'rgba(0, 0, 0, 1)', 
 	DOT_FILL_STYLE = 'rgba(255, 255, 255, 1)', 
 	DOT_SIZE = 3, 
-	BEAD_WIDTH = 48, 
+	BEAD_WIDTH = 56, 
 	BEAD_HEIGHT = 30, 
 	//BEAD_STROKE = 'rgba(128,51,0,1)', 
 	BEAD_STROKE = 'black',
@@ -49,11 +50,11 @@ Bead.prototype = {
 		points.push(new Point(center.x + BEAD_WIDTH / 2, center.y));
 		// ____
 
-		points.push(new Point(center.x + BEAD_WIDTH / 4, center.y - BEAD_HEIGHT / 2));
+		points.push(new Point(center.x + BEAD_WIDTH / 6, center.y - BEAD_HEIGHT / 2));
 		// ____\
 
 		//  __
-		points.push(new Point(center.x - BEAD_WIDTH / 4, center.y - BEAD_HEIGHT / 2));
+		points.push(new Point(center.x - BEAD_WIDTH / 6, center.y - BEAD_HEIGHT / 2));
 		// ____\
 
 		//   __
@@ -61,12 +62,12 @@ Bead.prototype = {
 		// /____\
 
 		//   __
-		points.push(new Point(center.x - BEAD_WIDTH / 4, center.y + BEAD_HEIGHT / 2));
+		points.push(new Point(center.x - BEAD_WIDTH / 6, center.y + BEAD_HEIGHT / 2));
 		// /____\
 		// \
 
 		//   __
-		points.push(new Point(center.x + BEAD_WIDTH / 4, center.y + BEAD_HEIGHT / 2));
+		points.push(new Point(center.x + BEAD_WIDTH / 6, center.y + BEAD_HEIGHT / 2));
 		// /____\
 		// \ __
 
@@ -79,19 +80,19 @@ Bead.prototype = {
 	},
 
 	evalPosition : function() {// returns the central point of the bead;
-		var x = MARGIN + this.rod * DISTANCE_RODS, y = undefined;
+		var x = MARGIN_LEFT + this.rod * DISTANCE_RODS, y = undefined;
 
 		if (this.heaven) {
 			if (this.active) {
-				y = MARGIN + HEAVEN - BEAD_HEIGHT / 2 - FRAME_LINE_WIDTH / 2;
+				y = MARGIN_TOP + HEAVEN - BEAD_HEIGHT / 2 - FRAME_LINE_WIDTH / 2;
 			} else {
-				y = MARGIN + BEAD_HEIGHT / 2 + FRAME_LINE_WIDTH / 2;
+				y = MARGIN_TOP + BEAD_HEIGHT / 2 + FRAME_LINE_WIDTH / 2;
 			}
 		} else {//earth
 			if (this.active) {
-				y = MARGIN + HEAVEN + (this.order - 1) * BEAD_HEIGHT + BEAD_HEIGHT / 2 + FRAME_LINE_WIDTH / 2;
+				y = MARGIN_TOP + HEAVEN + (this.order - 1) * BEAD_HEIGHT + BEAD_HEIGHT / 2 + FRAME_LINE_WIDTH / 2;
 			} else {
-				y = MARGIN + HEAVEN + this.order * BEAD_HEIGHT + BEAD_HEIGHT / 2 + FRAME_LINE_WIDTH / 2;
+				y = MARGIN_TOP + HEAVEN + this.order * BEAD_HEIGHT + BEAD_HEIGHT / 2 + FRAME_LINE_WIDTH / 2;
 			}
 
 		}
@@ -153,7 +154,7 @@ function drawAbacus() {
 	drawFrame();
 	drawHeavenLine();
 	drawDots();
-	valueElement.textContent = 'Value: ' + getAbacusValue();
+	valueElement.value	 = ' ' + getAbacusValue();
 }
 
 function drawFrame() {
@@ -161,7 +162,7 @@ function drawFrame() {
 	context.strokeStyle = frameColor;
 	context.lineWidth = FRAME_LINE_WIDTH;
 	context.beginPath();
-	context.rect(MARGIN, MARGIN, width, HEIGHT);
+	context.rect(MARGIN_LEFT, MARGIN_TOP, width, HEIGHT);
 	context.stroke();
 	context.restore();
 }
@@ -171,8 +172,8 @@ function drawHeavenLine() {
 	context.strokeStyle = frameColor;
 	context.lineWidth = FRAME_LINE_WIDTH;
 	context.beginPath();
-	context.moveTo(MARGIN + FRAME_LINE_WIDTH / 2, MARGIN + HEAVEN);
-	context.lineTo(MARGIN + width - FRAME_LINE_WIDTH / 2, MARGIN + HEAVEN);
+	context.moveTo(MARGIN_LEFT + FRAME_LINE_WIDTH / 2, MARGIN_TOP + HEAVEN);
+	context.lineTo(MARGIN_LEFT + width - FRAME_LINE_WIDTH / 2, MARGIN_TOP + HEAVEN);
 	context.stroke();
 	context.restore();
 }
@@ -181,10 +182,10 @@ function drawRods() {
 	context.save();
 	context.strokeStyle = ROD_STROKE_STYLE;
 	context.lineWidth = ROD_LINE_WIDTH;
-	for (var i = 0, x = MARGIN + DISTANCE_RODS; i < numberOfRods; ++i, x += DISTANCE_RODS) {
+	for (var i = 0, x = MARGIN_LEFT + DISTANCE_RODS; i < numberOfRods; ++i, x += DISTANCE_RODS) {
 		context.beginPath();
-		context.moveTo(x, MARGIN);
-		context.lineTo(x, MARGIN + HEIGHT);
+		context.moveTo(x, MARGIN_TOP);
+		context.lineTo(x, MARGIN_TOP + HEIGHT);
 		context.stroke();
 	}
 
@@ -199,11 +200,11 @@ function drawDots() {
 	context.fillStyle = DOT_FILL_STYLE;
 	context.shadowOffsetX = 0;
 	context.shadowOffsetY = 0;
-	for (var i = 0, x = MARGIN + DISTANCE_RODS; i < numberOfRods; ++i, x += DISTANCE_RODS) {
+	for (var i = 0, x = MARGIN_LEFT + DISTANCE_RODS; i < numberOfRods; ++i, x += DISTANCE_RODS) {
 		// Dot in this and this +- 3
 		if ((i - middle) % 3 === 0) {
 			context.beginPath();
-			context.arc(x, MARGIN + HEAVEN, DOT_SIZE, 0, Math.PI * 2, false);
+			context.arc(x, MARGIN_TOP + HEAVEN, DOT_SIZE, 0, Math.PI * 2, false);
 			context.fill();
 			context.stroke();
 		}
@@ -260,6 +261,23 @@ function getAbacusValue() {
 		}
 	});
 	return value.toFixed(numberOfRods - evalUnitsRod());
+}
+
+function putValueInAbacus(value) {
+	
+}
+
+function resetRod(rod) {
+	
+}
+
+function putNumberInRod(number, rod) {
+	resetRod(rod);
+	if (number < 5) {
+		for (var i = 0; i < number; i++) {
+			
+		}
+	}
 }
 
 
